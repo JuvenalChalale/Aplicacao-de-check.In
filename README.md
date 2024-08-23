f78096aff7e1b32b90108cbe2703f6f5c0eff444
Juvenal Chalale <juvenalchalale@gmail.com>
2024-08-23 13:15:26 +0100

    Commitando o projecto completo

---
+++ README.md
@@ -0,0 +1,76 @@
+<p align="center">
+  <img alt="Logo NLW Unite - Rocketseat" src=".github/logo.png" width="200px" />
+  <h1 align="center">NLW Unite - Trilha Fullstack</h1>
+</p>
+
+<p align="center">
+  <a alt="HTML5">
+    <img src="https://img.shields.io/badge/HTML5-E34F26?logo=html5&logoColor=fff&style=for-the-badge" />
+  </a>
+  <a alt="CSS3">
+    <img src="https://img.shields.io/badge/CSS3-1572B6?logo=css3&logoColor=fff&style=for-the-badge" />
+  </a>
+  <a alt="Javascript">
+    <img src="https://img.shields.io/badge/JAVASCRIPT-F7DF1E?logo=javascript&logoColor=000&style=for-the-badge" />
+  </a>
+  <a alt="TailwindCSS">
+    <img src="https://img.shields.io/badge/TAILWINDCSS-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white" />
+  </a>
+  <a alt="Figma">
+     <img src="https://img.shields.io/badge/Figma-F24E1E?logo=figma&logoColor=white&style=for-the-badge" />
+  </a>
+</p>
+
+<p align="center">
+  <a href="#tecnologias">Tecnologias</a> •
+  <a href="#projeto">Projeto</a> •
+  <a href="#extras">Extras</a> •
+  <a href="#layout">Layout</a> •  
+  <a href="#licenca">Licença</a>
+</p>
+
+<br>
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=F48F56&labelColor=00292E">
+</p>
+
+<p align="center">
+  <img alt="Preview do projeto desenvolvido." src=".github/preview.png" width="80%">
+</p>
+
+<h4 align="center">
+  
+  [Ver projeto](https://gabrielcenteiofreitas.github.io/estudos-rocketseat-nlw15_unite-trilha_fullstack/)
+</h4>
+
+## 🚀 Tecnologias<a id="tecnologias"></a>
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- Javascript
+- Tailwind CSS
+- Figma
+
+## 💻 Projeto<a id="projeto"></a>
+
+Projeto de uma versão simplificada de um sistema de check-in para eventos presenciais desenvolvido durante o evento "NLW Unite" da Rocketseat na Trilha Fullstack(HTML+CSS+JS) com o objetivo de treinar e aprimorar habilidades.
+
+Caso queira ver o projeto clique [aqui](https://gabrielcenteiofreitas.github.io/estudos-rocketseat-nlw15_unite-trilha_fullstack/).
+
+## ⭐ Extras<a id="extras"></a>
+
+Uma lista de coisas extras que fiz durante o projeto:
+
+- Recriei todo o projeto utilizando Tailwind CSS
+- Melhorei a responsividade
+- Adicionei animações de hover e focus
+
+## 🔖 Layout<a id="layout"></a>
+
+Você pode visualizar o layout do projeto através [DESSE LINK](https://www.figma.com/community/file/1356738933008624188).
+
+## 📝 Licença<a id="licenca"></a>
+
+Esse projeto está sob a licença MIT.
---
+++ src/script.js
@@ -0,0 +1,151 @@
+let participantes = [
+  {
+    nome: "Juvenal Chalale",
+    email: "juvenalchalale@gmail.com",
+    dataInscricao: new Date(2024, 2, 22, 19, 20),
+    dataCheckIn: new Date(2024, 2, 25, 22, 10),
+  },
+  {
+    nome: "Mayk Brito",
+    email: "gabriel@gmail.com",
+    dataInscricao: new Date(2024, 2, 23, 15, 10),
+    dataCheckIn: null,
+  },
+  {
+    nome: "Carmen Varela",
+    email: "4c@gmail.com",
+    dataInscricao: new Date(2024, 2, 25, 10, 30),
+    dataCheckIn: new Date(2024, 2, 27, 14, 20),
+  },
+  {
+    nome: "Esperanca Chalale",
+    email: "espcom@gmail.com",
+    dataInscricao: new Date(2024, 2, 26, 12, 45),
+    dataCheckIn: null,
+  },
+  {
+    nome: "Raquel Chalale",
+    email: "rcc@gmail.com",
+    dataInscricao: new Date(2024, 2, 27, 8, 0),
+    dataCheckIn: new Date(2024, 2, 29, 10, 40),
+  },
+  {
+    nome: "Justino Chalale",
+    email: "justino@gmail.com",
+    dataInscricao: new Date(2024, 2, 28, 16, 20),
+    dataCheckIn: new Date(2024, 3, 1, 20, 5),
+  },
+  {
+    nome: "Belarmino Chalale",
+    email: "belarmino@gmail.com",
+    dataInscricao: new Date(2024, 2, 29, 11, 55),
+    dataCheckIn: new Date(2024, 3, 2, 15, 30),
+  },
+  {
+    nome: "Pedro Almeida",
+    email: "pedro@gmail.com",
+    dataInscricao: new Date(2024, 3, 1, 14, 40),
+    dataCheckIn: null,
+  },
+  {
+    nome: "Valentino Chalale",
+    email: "valentino@gmail.com",
+    dataInscricao: new Date(2024, 3, 2, 9, 15),
+    dataCheckIn: null,
+  },
+  {
+    nome: "Mario Mudiquixi",
+    email: "Mario@gmail.com",
+    dataInscricao: new Date(2024, 3, 3, 18, 5),
+    dataCheckIn: new Date(2024, 3, 5, 21, 35),
+  }
+];
+
+
+const criarNovoParticipante = (participante) => {
+  const dataInscricao = dayjs(Date.now()).to(participante.dataInscricao)
+
+  let dataCheckIn = dayjs(Date.now()).to(participante.dataCheckIn)
+  
+  if(participante.dataCheckIn == null) {
+    dataCheckIn = `
+      <button
+        class="font-normal text-xs text-nlw-light-green hover:underline"
+        data-email="${participante.email}"
+        onclick="fazerCheckIn(event)"
+      >
+        Confirmar check-in
+      </button>
+    `
+  }
+
+  return `
+  <tr>
+    <td class="px-4 py-3 flex flex-col gap-1 border-t border-t-white/10">
+      <strong class="font-semibold text-sm text-white">
+        ${participante.nome}
+      </strong>
+      <small class="font-normal text-xs text-nlw-grey">
+        ${participante.email}
+      </small>
+    </td>
+    <td class="px-4 py-3 font-normal text-xs text-nlw-grey border-t border-t-white/10">${dataInscricao}</td>
+    <td class="px-4 py-3 font-normal text-xs text-nlw-grey border-t border-t-white/10">${dataCheckIn}</td>
+  </tr>
+  `
+}
+
+const atualizarLista = (participantes) => {
+  let output = ""
+  for(let participante of participantes) {
+    output = output + criarNovoParticipante(participante)
+  }
+
+  document.querySelector('tbody').innerHTML = output
+}
+
+atualizarLista(participantes)
+
+const adicionarParticipante = (event) => {
+  event.preventDefault()
+
+  const dadosDoFormulario = new FormData(event.target)
+
+  const participante = {
+    nome: dadosDoFormulario.get('nome'),
+    email: dadosDoFormulario.get('email'),
+    dataInscricao: new Date(),
+    dataCheckIn: null  
+  }
+
+  const participanteExiste = participantes.find(
+    (p) => p.email == participante.email
+  )
+
+  if(participanteExiste) {
+    alert('Email já cadastrado!')
+    return
+  }
+
+  participantes = [participante, ...participantes]
+  atualizarLista(participantes)
+
+  event.target.querySelector('[name="nome"]').value = ""
+  event.target.querySelector('[name="email"]').value = ""
+}
+
+const fazerCheckIn = (event) => {
+  const mensagemConfirmacao = 'Tem certeza que deseja fazer o check-in?' 
+
+  if(confirm(mensagemConfirmacao) == false) {
+    return
+  }
+
+  const participante = participantes.find(
+    (p) => p.email == event.target.dataset.email  
+  )
+  
+  participante.dataCheckIn = new Date()
+
+  atualizarLista(participantes)
+}
